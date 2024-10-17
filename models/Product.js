36cdf0c37184cd3e0d89023b9f751a520a9c5c0d
BrianSechelo <sechbrian123.gmail.com>
2024-10-18 00:22:03 +0300

    Add dependencies for MongoDb and Edit Shop and Cart Pages to capture Cart Items and their totals

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,7 @@ const productSchema = new mongoose.Schema({
         type: String,
         required: true,
     },
-    price: {
+    price: {l
         type: Number,
         required: true,
     },
